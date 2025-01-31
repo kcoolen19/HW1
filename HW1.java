2117c48fc838b56033fe750a030a34df6c38e752
kcoolen19 <kcoolen19@gmail.com>
2025-01-30 21:57:36 -0600

    Adding answers

--- HW1.java
+++ HW1.java
@@ -86,9 +86,25 @@ public class HW1 {
          * found in the linked-list that is less than thr parameter value passed.
          */
         public void removeElementsLT ( int ltValue ) {
-
-            // YOUR CODE GOES HERE
-
+            // Check if the value of the head node is less than the parameter
+            while (head != null && head.data < ltValue) {
+                head = head.next;
+            }
+            // A node to track the current node
+            Node current = head;
+            // Traverse the linked list starting from the head 
+            while (current.next != null) {
+                /*
+                 If the value of the next node is less than the parameter,
+                  the pointer to the original next node is moved to the node after that 
+                  Otherwise, the next node is checked
+                */ 
+                if (current.next.data < ltValue) {
+                    current.next = current.next.next;
+                } else {
+                    current = current.next;
+                }
+            }
             return;
         }
 
@@ -99,9 +115,25 @@ public class HW1 {
          */
 
         public void removeElement ( int value ) {
-
-            // YOUR CODE GOES HERE
-
+            // Check if the value of the head node is equal to the parameter
+            while (head != null && head.data == value) {
+                head = head.next;
+            }
+            // A node to track the current node
+            Node current = head;
+            // Traverse the linked list starting from the head 
+            while (current.next != null) {
+                /*
+                 If the value of the next node is equal to the parameter,
+                  the pointer to the original next node is moved to the node after that
+                 Otherwise, the next node is checked
+                 */
+                if (current.next.data == value) {
+                    current.next = current.next.next;
+                } else {
+                    current = current.next;
+                }
+            }
             return;
         }
 
@@ -156,12 +188,35 @@ public class HW1 {
          * The method should utilize the provided Stack class.
          */
         public static boolean isPalindrome(String input) {
-
             Stack<Character> stack = new Stack<>();
             input = input.toLowerCase().replaceAll("\\s+", "");
-
-            // Your CODE GOES HERE
-            return false;
+            // The word is split into the individual characters and stored in an array
+            char[] inputCharacters = input.toCharArray();
+            // The length of the array is stored in a variable 
+            int length = inputCharacters.length;
+            // Finding midpoint of the characters array
+            int midpoint = length / 2;
+            // The first half of the characters array is pushed into the stack
+            for (int i = 0; i < midpoint; i++) {
+                stack.push(inputCharacters[i]);
+            }
+            // If the length of the word is not even, the midpoint is incremented
+            if (length % 2 == 1) {
+                midpoint++;
+            }
+            /*
+             A while loop is run from the midpoint value to the last element of the characters array
+             The letters popped are compared with the first half of the characters array
+             The word is a palindrome if the letters popped are identical to the first half of the stack
+             */
+            while (midpoint < length) {
+                if (stack.pop() == inputCharacters[midpoint]) {
+                    return true; 
+                }
+                midpoint++;
+            }
+            // The word is not palindrome otherwise
+            return false; 
         }
 
 
@@ -180,11 +235,40 @@ public class HW1 {
          * pop elements off the passed in stack, place them in a temp stack. Then when
          * completed, place them all back in teh original stack.
          */
-        public static int findLargestK(Stack<Integer> stack, int k) {
-
-            // YOUR CODE GOES HERE
-            return -1;
+         public static int findLargestK(Stack<Integer> stack, int k) {
+        // Create a temporary stack to store the number popped from the original stack
+        Stack<Integer> tempStack = new Stack<>();
+        // Variable to track the largest index of k
+        int largestIndex = -1;
+        // Variable to monitor the size of the original stack as the number are being popped
+        int currentIndex = stack.size() - 1; 
+        
+    
+        /*
+         The stack is traversed
+         The numbers are popped one by one and stored in the variable, current
+         When the current value popped matches parameter k and is greater than the largest index,
+         The largest index becomes the current index reached
+         The number popped is pushed in the temporary stack
+         As the numbers are popped, the current index is decremented to 0
+         */
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            if (current == k && currentIndex > largestIndex) {
+                largestIndex = currentIndex;  
+            }
+            tempStack.push(current);
+            currentIndex--;  
         }
+        // The original stack is restored by pushing the values popped from the temporary stack
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
+        // The largest index is returned
+        return largestIndex;
+        }
+        
+
 
     }  // End class Stacks
 
@@ -219,7 +303,7 @@ public class HW1 {
         */
 
         // RETURN THE CORRECT OPTION NUMBER LISTED ABOVE
-        return -1;
+        return 3;
     }
 
 
@@ -240,7 +324,7 @@ public class HW1 {
          */
 
         // RETURN THE CORRECT OPTION LISTED ABOVE
-        return -1;
+        return 2;
     }
 
 }
